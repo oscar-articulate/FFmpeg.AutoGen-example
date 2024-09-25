6b240eff6451ee8220953141b1c67560e07e18fc
Oscar Romero <oromero@articulate.com>
2024-09-25 10:04:57 -0700

    Add project files.

---
+++ FFmpeg.AutoGen.Example/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace FFmpeg.AutoGen.Example
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            FFmpegBinariesHelper.RegisterFFmpegBinaries();
+
+            DecodeAllFramesToImages();
+        }
+
+
+        private static unsafe void DecodeAllFramesToImages()
+        {
+            // hard-coding for debug
+
+            var url = "C:\\\\Users\\\\Oscar Romero\\\\AppData\\\\Local\\\\Temp\\\\Articulate\\\\Storyline\\\\5dASEOL8dBc\\\\5xbhN9K4QNQ.mp4"; // be advised this file holds 1440 frames
+
+
+            using (var vsd = new VideoStreamDecoder(url))
+            {
+                var sourceSize = vsd.FrameSize;
+                var sourcePixelFormat = AVPixelFormat.AV_PIX_FMT_NONE;
+                var destinationSize = sourceSize;
+                var destinationPixelFormat = AVPixelFormat.AV_PIX_FMT_BGR24;
+                using (var vfc = new VideoFrameConverter(sourceSize,
+                           sourcePixelFormat,
+                           destinationSize,
+                           destinationPixelFormat))
+                {
+                    var stopwatch = Stopwatch.StartNew();
+
+
+                    // todo: implement seeking
+
+                    var frameNumber = 0;
+                    while (vsd.TryDecodeNextFrame(out var frame) && frameNumber < 25)
+                    {
+
+                        var convertedFrame = vfc.Convert(frame);
+
+                        var width = convertedFrame.width;
+                        var height = convertedFrame.height;
+
+                        var format = PixelFormat.Format24bppRgb;
+                        var scan0 = (IntPtr)convertedFrame.data[0];
+                        var stride = convertedFrame.linesize[0];
+
+                        using (var bitmap = new Bitmap(width, height, stride, format, scan0))
+                            bitmap.Save($"frame.{frameNumber:D8}.jpg", ImageFormat.Jpeg);
+
+                        frameNumber++;
+
+                        stopwatch.Stop();
+                        Console.WriteLine($"Generated image in: {stopwatch.Elapsed.ToString()}");
+                        stopwatch = Stopwatch.StartNew();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FFmpeg.AutoGen.Example/VideoStreamDecoder.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+
+namespace FFmpeg.AutoGen.Example
+{
+    public sealed unsafe class VideoStreamDecoder : IDisposable
+    {
+        private readonly AVCodecContext* _pCodecContext;
+        private readonly AVFormatContext* _pFormatContext;
+        private readonly int _streamIndex;
+        private readonly AVFrame* _pFrame;
+        private readonly AVFrame* _receivedFrame;
+        private readonly AVPacket* _pPacket;
+
+        public VideoStreamDecoder(string url)
+        {
+            _pFormatContext = ffmpeg.avformat_alloc_context();
+            _receivedFrame = ffmpeg.av_frame_alloc();
+            var pFormatContext = _pFormatContext;
+            ffmpeg.avformat_open_input(&pFormatContext, url, null, null).ThrowExceptionIfError();
+            ffmpeg.avformat_find_stream_info(_pFormatContext, null).ThrowExceptionIfError();
+            AVCodec* codec = null;
+            _streamIndex = ffmpeg.av_find_best_stream(_pFormatContext, AVMediaType.AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0).ThrowExceptionIfError();
+            _pCodecContext = ffmpeg.avcodec_alloc_context3(codec);
+
+            ffmpeg.avcodec_parameters_to_context(_pCodecContext, _pFormatContext->streams[_streamIndex]->codecpar).ThrowExceptionIfError();
+            ffmpeg.avcodec_open2(_pCodecContext, codec, null).ThrowExceptionIfError();
+
+            CodecName = ffmpeg.avcodec_get_name(codec->id);
+            FrameSize = new Size(_pCodecContext->width, _pCodecContext->height);
+            PixelFormat = _pCodecContext->pix_fmt;
+
+            _pPacket = ffmpeg.av_packet_alloc();
+            _pFrame = ffmpeg.av_frame_alloc();
+        }
+
+        public string CodecName { get; }
+        public Size FrameSize { get; }
+        public AVPixelFormat PixelFormat { get; }
+
+        public void Dispose()
+        {
+            ffmpeg.av_frame_unref(_pFrame);
+            ffmpeg.av_free(_pFrame);
+
+            ffmpeg.av_packet_unref(_pPacket);
+            ffmpeg.av_free(_pPacket);
+
+            ffmpeg.avcodec_close(_pCodecContext);
+            var pFormatContext = _pFormatContext;
+            ffmpeg.avformat_close_input(&pFormatContext);
+        }
+
+        public bool TryDecodeNextFrame(out AVFrame frame)
+        {
+            ffmpeg.av_frame_unref(_pFrame);
+            ffmpeg.av_frame_unref(_receivedFrame);
+            int error;
+            do
+            {
+                try
+                {
+                    do
+                    {
+                        error = ffmpeg.av_read_frame(_pFormatContext, _pPacket);
+                        if (error == ffmpeg.AVERROR_EOF)
+                        {
+                            frame = *_pFrame;
+                            return false;
+                        }
+
+                        error.ThrowExceptionIfError();
+                    } while (_pPacket->stream_index != _streamIndex);
+
+                    ffmpeg.avcodec_send_packet(_pCodecContext, _pPacket).ThrowExceptionIfError();
+                }
+                finally
+                {
+                    ffmpeg.av_packet_unref(_pPacket);
+                }
+
+                error = ffmpeg.avcodec_receive_frame(_pCodecContext, _pFrame);
+            } while (error == ffmpeg.AVERROR(ffmpeg.EAGAIN));
+            error.ThrowExceptionIfError();
+            if (_pCodecContext->hw_device_ctx != null)
+            {
+                ffmpeg.av_hwframe_transfer_data(_receivedFrame, _pFrame, 0).ThrowExceptionIfError();
+                frame = *_receivedFrame;
+            }
+            else
+            {
+                frame = *_pFrame;
+            }
+            return true;
+        }
+    }
+}
